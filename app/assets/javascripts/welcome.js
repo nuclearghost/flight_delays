5f3df4a0c2387d9ed8c5b3bfcc7d6615670821bd
Mark Meyer <mark.meyer37@gmail.com>
2013-12-01 03:27:50 +0000

    wrapped up styling

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -2,10 +2,15 @@ $(function(){
   $('#find-btn').click(function(){
     var from = $('#from').val(),
         to = $('#to').val();
+    var el = $("#delay-box h3").text("");
+
     $.ajax("/delay.json?from=" + from + "&to=" + to)
       .done(function(data) {
-        console.log(data);
-        $("#delay-box span").text(data.delay);
+        if (data.delay) {
+          el.text(data.delay);
+        } else {
+          el.text("No Data Available");
+        }
       });
   });
 });
